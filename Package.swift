020f1429c424a40b2c732d34d4d85beab2e9bcfd
Alexey Pustobaev <L@whisperlab.com>
2016-05-09 16:12:09 +0300

    rm GCDAsyncSocket --> BlueSocket

--- Package.swift
+++ Package.swift
@@ -3,5 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Rethink",
     targets: [Target(name: "Rethink")],
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 5),
+    ],
     exclude: ["Rethink.xcodeproj", "Rethink.xcworkspace", "README.md", "LICENSE", "Sources/Info.plist", "Tests"]
 )
